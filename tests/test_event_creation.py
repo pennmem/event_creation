25b7a529442b6a0ec71e68ce105ccfd0f44ba0ce
iped <iped@sas.upenn.edu>
2016-10-07 11:13:43 -0400

    C01 now matches C1

--- tests/test_event_creation.py
+++ tests/test_event_creation.py
@@ -861,7 +861,7 @@ def ps_stim_comparison_exceptions(event1, event2, field_name1, field_name2):
     field2 = StimComparator.get_subfield(event2, field_name2)[0]
 
     if field_name1 in ('stim_params.anode_label', 'stim_params.cathode_label') and \
-                    (field1[:-1] + '0' + field1[-1]) == field2:
+                    len(field1)>0 and (field1[:-1] + '0' + field1[-1]) == field2:
         return True
 
     if field_name1 == 'stim_params.amplitude' and field2 * 1000 == field1:
