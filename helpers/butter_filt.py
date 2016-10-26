4ef19c3e191e89b4883e80c7f29228a1a6430d00
jpazdera <jpazdera@haverford.edu>
2016-10-26 15:44:37 -0400

    Butter filt adjusted to work properly with frequency ranges

--- helpers/butter_filt.py
+++ helpers/butter_filt.py
@@ -1,7 +1,8 @@
+import numpy as np
 from scipy.signal import butter, filtfilt
 
 
-def butter_filt(data, freq_range=(58, 62), sample_rate=256, filt_type='bandstop', order=4):
+def butter_filt(data, freq_range, sample_rate=256, filt_type='bandstop', order=4):
     """
     Designs and runs an Nth order digital butterworth filter on an array of data.
 
@@ -10,7 +11,7 @@ def butter_filt(data, freq_range=(58, 62), sample_rate=256, filt_type='bandstop'
     scripts if not set to 3.
 
     :param data: An array containing the data to be filtered
-    :param freq_range: The range of the filter
+    :param freq_range: A single frequency to filter on or a 2D matrix where each row is a frequency range to filter on
     :param sample_rate: The sampling rate of the EEG recording
     :param filt_type: The type of filter to run - can be 'bandstop', 'highpass', 'lowpass', or 'bandpass'
     :param order: The order of the filter
@@ -18,9 +19,11 @@ def butter_filt(data, freq_range=(58, 62), sample_rate=256, filt_type='bandstop'
     """
     # Calculate Nyquist frequency
     nyq = sample_rate/2.
+    # Convert the frequency range to a 2D matrix if it was input as an integer
+    freq_range = np.array([[freq_range]]) if isinstance(freq_range, (int, float)) else np.array(freq_range)
+    freq_range = freq_range / nyq
     # Get the Butterworth values and run the filter for zero phase distortion
-    freq_range = [freq_range] if isinstance(freq_range, (int, float)) else freq_range
-    for i in range(len(freq_range)):
-        Bb, Ab = butter(order, freq_range[i]/nyq, btype=filt_type)
+    for i in range(np.size(freq_range), 0):
+        Bb, Ab = butter(order, freq_range[i, :], btype=filt_type)
         data = filtfilt(Bb, Ab, data, padlen=3)  # PADLEN MUST BE SET TO 3 TO MATCH MATLAB IMPLEMENTATION
     return data
