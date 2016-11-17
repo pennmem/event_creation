80ebd5b6e11dc363887f6e76d8931df4c1363248
iped <iped@sas.upenn.edu>
2016-11-17 17:23:23 -0500

    config points explicitly to RAM_maint's account for loc_DB

--- submission/config.py
+++ submission/config.py
@@ -1,9 +1,9 @@
 import os
 
-RHINO_ROOT = '/' #os.path.join(os.environ['HOME'], 'rhino_mount')
+RHINO_ROOT = '/'
 DATA_ROOT=os.path.join(RHINO_ROOT, 'data/eeg')
-LOC_DB_ROOT=os.path.expanduser('~/stim/')
+LOC_DB_ROOT= os.path.join(RHINO_ROOT, 'home2', 'RAM_maint', 'stim')
 #DB_ROOT='/Volumes/db_root/' #os.path.join(RHINO_ROOT, 'data', 'eeg')
-DB_ROOT=os.path.expanduser('/')
+DB_ROOT=RHINO_ROOT
 EVENTS_ROOT=os.path.join(RHINO_ROOT, 'data/events')
 
