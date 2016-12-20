0738c26766ed155e6a8daafdffdfa5b0abf0e4cd
iped <iped@sas.upenn.edu>
2016-12-20 16:38:23 -0500

    Documentation

--- Localization.py
+++ Localization.py
@@ -11,6 +11,7 @@ class Localization(object):
             self.from_json(json_file)
 
     def from_json(self, json_file):
+        """ Loads from a json file """
         self._contact_dict = json.load(open(json_file))
         self._orig_filename = json_file
         for lead_name, lead in self._contact_dict['leads'].items():
@@ -26,25 +27,52 @@ class Localization(object):
                     lead['pairs'].append(pair)
 
     def to_json(self, json_file):
+        """ Dumps to a json file """
         clean_json_dump(self._contact_dict, open(json_file, 'w'), indent=2, sort_keys=True)
 
     def get_lead_names(self):
         """ Returns list of strings of lead names """
         return list(self._contact_dict['leads'].keys())
 
-    def get_lead_type(self, leads):
+    def get_lead_type(self, lead):
+        """ Gets the type of the lead ("S", "G" or "D")
+        :param lead: the name of a lead
+        :return: "S", "G", or "D"
         """
-        Returns the type of contact ("S", "G", or "D") for a given contact or lead
-        """
-        return self._contact_dict['leads'][leads]['type']
+        if lead in self._contact_dict['leads']:
+            return self._contact_dict['leads'][lead]['type']
+        else:
+            raise Exception("Lead {} does not exist".format(lead))
 
     def get_lead_types(self, leads):
-        return [self._contact_dict['leads'][lead]['type'] for lead in leads]
+        """ Gets the types for each lead
+        :param leads: a list of lead names
+        :returns: a list of types ("S", "G", or "D")
+        """
+        return [self.get_lead_type(lead) for lead in leads]
+
+    def get_contact_type(self, contact):
+        """ Gets the type of a contact
+        :param contact: The name of the contact
+        :returns: "S", "G" or "D"
+        """
+        for lead in self._contact_dict['leads'].values():
+            for contact_dict in lead['contacts']:
+                if contact_dict['name'] == contact:
+                    return lead['type']
+        raise Exception("Contact {} does not exist".format(contact))
+
+    def get_contact_types(self, contacts):
+        """ Gets the types for each provided contact
+        :param contacts: A list of contact names
+        :returns: a list of types ("S", "G", or "D")
+        """
+        return [self.get_contact_type(contact) for contact in contacts]
 
     def get_contacts(self, leads=None):
-        """ 
-        Returns list of strings of contacts
-        for each lead if provided, otherwise all contacts
+        """ Gets the names of contacts
+        :param leads: (optional) the lead or leads on which to find the contacts. If not provided finds on all leads
+        :return: a list of contact names
         """
         if leads is None:
             leads = self.get_lead_names()
@@ -57,10 +85,10 @@ class Localization(object):
         return contacts
 
     def get_contact_coordinate(self, coordinate_space, contact):
-        """
-        coord_type: one of "fs", "t1_mri", "ct_voxels", ...
-        contacts: string or list of strings of contact names
-        returns: np.array of coordinates for each contact
+        """ Gets the coordinates for a single contact
+        :param coordinate_space: one of "fs", "t1_mri", "ct_voxels", ...
+        :param contact: name of the contact
+        :returns: np.array of [x,y,z] for contact
         """
 
         contact_dict = self._contact_dict_by_name(contact)
@@ -69,25 +97,40 @@ class Localization(object):
         return np.array(contact_dict['coordinate_spaces'][coordinate_space])
 
     def get_contact_coordinates(self, coordinate_space, contacts):
+        """ Gets the coordinates for all provided contacts
+        :param coordinate_space: one of "fs", "t1_mri", "ct_voxels"...
+        :param contacts: list of contact names
+        :returns: np.array of [[x1, y1, z1], [x2, y2, z2], ...] for each contact
+        """
         coordinates = []
         for contact in contacts:
             coordinates.append(self.get_contact_coordinate(coordinate_space, contact))
         return np.array(coordinates)
     
     def set_contact_coordinate(self, coordinate, coordinate_space, contact):
-        """
-        Sets the coordinates for a given contact and coordinate type
+        """ Sets the coordinates for a given contact and coordinate space
+        :param coordinate: [x, y, z] of this contact's coordinates
+        :param coordinate_space: one of "fs", "t1_mri", "ct_voxels"...
+        :param contact: name of contact to modify
         """
         contact_dict = self._contact_dict_by_name(contact)
         contact_dict['coordinate_spaces'][coordinate_space] = [float(c) for c in coordinate]
 
     def set_contact_coordinates(self, coordinates, coordinate_space, contacts):
-
+        """ Sets the coordinates for all provided contacts
+        :param coordinates: [[x1, y1, z1], [x2, y2, z2], ...] for each contact
+        :param coordinate_space: one of "fs", "t1_mri", "ct_voxels"...
+        :param contacts: list of contact names (same length as coordinates)
+        """
         for contact, coordinate in zip(contacts, coordinates):
             self.set_contact_coordinate(coordinate, coordinate_space, contact)
 
     def get_contact_label(self, atlas_name, contact):
-
+        """ Gets the atlas label for a given contact
+        :param atlas_name: name of atlas to look up
+        :param contact: name of contact to look up
+        :return: atlas label if atlas exists for this contact, otherwise None
+        """
         contact = self._contact_dict_by_name(contact)
         if atlas_name in contact['atlases']:
             return contact['atlases'][atlas_name]
@@ -95,26 +138,38 @@ class Localization(object):
             return None
 
     def get_contact_labels(self, atlas_name, contacts):
+        """ Gets the atlas labels for each provided contact
+        :param atlas_name: name of atlas to look up
+        :param contacts: list of strings of contact names
+        :return: [label1, label2, ...] for each contact. Label is set to None if not yet assigned
+        """
         labels = []
         for contact_name in contacts:
             labels.append(self.get_contact_label(atlas_name, contact_name))
         return labels
 
     def set_contact_label(self, atlas_name, contact, label):
-        """
-        Sets the label for a given contact
+        """Sets the atlas label for a given contact
+        :param atlas_name: name of the atlas to set
+        :param contact: Name of the contact to set
+        :param label: atlas label to apply
         """
         contact_dict = self._contact_dict_by_name(contact)
         contact_dict['atlases'][atlas_name] = label
 
     def set_contact_labels(self, atlas_name, contacts, labels):
+        """ Sets the atlas labels for a list of contacts
+        :param atlas_name: name of the atlas to set
+        :param contacts: list of names of the contacts to change
+        :param labels: list of labels, same size as contacts
+        """
         for contact_name, label in zip(contacts, labels):
             self.set_contact_label(atlas_name, contact_name, label)
 
     def get_pairs(self, leads=None):
-        """
-        Returns a list of 2-tuples of strings with
-        contacts names that form a pair
+        """ Gets the names of adjacent contacts
+        :params leads: (optional) The lead or leads on which to find the pairs. If not provided finds all pairs.
+        :returns: list of 2-tuples of contact names representing adjacent electrodes
         """
         if leads is None:
             leads = self.get_lead_names()
@@ -128,10 +183,10 @@ class Localization(object):
         return pair_names
 
     def get_pair_coordinate(self, coordinate_space, pair):
-        """
-        coord_type: one of "fs", "t1_mri", "ct_voxels", ...
-        pairs: Nx2 list of contact names (output of get_pairs)
-        returns: np.array of coordinates for each pair
+        """ Gets the coordinates at which a pair is located
+        :param coordinate_space: one of "fs", "t1_mri", "ct_voxels"...
+        :param pair: 2 element list containing the names of the contacts in the pair
+        :returns: np.array of [x,y,z] for the provided pair (average of the provided coordinates)
         """
         contact_name1 = pair[0]
         contact_name2 = pair[1]
@@ -143,6 +198,11 @@ class Localization(object):
         return np.array(coord)
 
     def get_pair_coordinates(self, coordinate_space, pairs):
+        """ Gets the coordinates at which a set of pairs are located
+        :param coordinate_space: one of "fs", "t1_mri", "ct_voxels"...
+        :param pairs: 2xN list containing the names of contacts in each pair
+        :returns: np.array of [[x1,y1,z1],[x2,y2,z2]] for the provided pairs
+        """
         coords = []
         for pair in pairs:
             coords.append(self.get_pair_coordinate(coordinate_space, pair))
@@ -150,24 +210,38 @@ class Localization(object):
 
 
     def set_pair_label(self, atlas_name, pair, label):
-        """
-        Sets an atlas label for a given pair or list of pairs
+        """ Sets an atlas label for a given pair
+        :param atlas_name: one of "fs", "t1_mri", "ct_voxels"...
+        :param pair: 2 element list with the names of the contacts in the pair
+        :param label: atlas label to set
         """
         pair_dict = self._pair_dict_by_name(pair)
         pair_dict['atlases'][atlas_name] = label
 
     def get_pair_label(self, atlas_name, pair):
+        """ Gets an atlas label for a given pair
+        :param atlas_name: one of "fs", "t1_mri", "ct_voxels"...
+        :param pair: 2-element list with the names of the contacts in the pair
+        :returns: atlas label for the pair, or None if atlas does not exist
+        """
         pair = self._pair_dict_by_name(pair)
         if atlas_name in pair['atlases']:
             return pair['atlases'][atlas_name]
         return None
 
     def get_pair_labels(self, atlas_name, pairs):
+        """ Gets the atlas labels for a set of pairs
+        :param atlas_name: one of "fs", "t1_mri", "ct_voxels"...
+        :param pairs: 2xN list with the names of the contacts in the pairs
+        :returns: list of atlas labels. Labels are None where pair does not have that atlas assigned
+        """
         return [self.get_pair_label(atlas_name, pair) for pair in pairs]
 
     def set_pair_labels(self, atlas_name, pairs, labels):
-        """
-        Sets an atlas label for a given pair or list of pairs
+        """ Sets the atlas labels for a set of pairs
+        :param atlas_name: one of "fs", "t1_mri", "ct_voxels"...
+        :param pairs: 2xN list with the names of the contacts in the pairs
+        :param labels: N-element list with the proper atlas labels to set
         """
         for pair, label in zip(pairs, labels):
             self.set_pair_label(atlas_name, pair, label)
@@ -219,6 +293,11 @@ if __name__ == '__main__':
     print('Leads {} have contacts {}'.format(leads[0:3], contact_names_multi))
     contact_names_all = loc.get_contacts()
     print('All contacts are {}'.format(contact_names_all))
+
+    type1 = loc.get_contact_type(contact_names1[0])
+    print("Contact {} has type {}".format(contact_names1[0], type1))
+    types_multi = loc.get_contact_types(contact_names_all[:5])
+    print("Contacts {} have types {}".format(contact_names_all[:5], types_multi))
 
     coords1 = loc.get_contact_coordinate('ct_voxel', contact_names1[0])
     print('Lead {} has coordinate {}'.format(contact_names1[0], coords1))
