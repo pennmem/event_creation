e033236ca2ed03a332a54fddaf6559c1b02fcd93
iped <iped@sas.upenn.edu>
2017-01-23 11:02:46 -0500

    removed misplaced neurorad stuff

--- cleanup/config.py
+++
@@ -1,8 +0,0 @@
-"""
-To run snap_coordinates from your local machine, set the path to 
-the root of mounted rhino. 
-
-e.g. if rhino is mounted in a folder named RHINO in /Volumes, set
-RHINO_ROOT='/Volumes/RHINO'
-"""
-RHINO_ROOT='/'
