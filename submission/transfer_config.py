aa7b40bf985db6ca1ba7dcaa16abb62ce8e86afe
jpazdera <jpazdera@haverford.edu>
2017-07-28 11:07:45 -0400

    Added exception to checksum for .mff "files", which are actually packages

--- submission/transfer_config.py
+++ submission/transfer_config.py
@@ -256,7 +256,11 @@ class TransferFile(object):
                 contents.append(os.path.basename(filename))
         else:
             for filename in self.origin_paths:
-                contents.append(open(filename).read())
+                if filename.endswith('.mff'):
+                    # JPazdera: Added this to condition to prevent .mff EEG packages from being opened as files
+                    contents.append(os.path.basename(filename))
+                else:
+                    contents.append(open(filename).read())
 
         for file in self.files.values():
             contents.extend(file.contents_to_check())
