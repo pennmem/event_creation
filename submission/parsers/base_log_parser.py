d5e7854702b87cdeb0b706bb3b75254108587300
leond <lad258@cornell.edu>
2017-12-05 16:04:56 -0500

    By request, parser now raises an error if an annotation file is missing

--- submission/parsers/base_log_parser.py
+++ submission/parsers/base_log_parser.py
@@ -11,6 +11,7 @@ from ..log import logger
 from ..exc import LogParseError, UnknownExperimentError, EventFieldError
 from ..readers.eeg_reader import read_jacksheet
 from ..viewers.recarray import pformat_rec, to_dict, from_dict
+from ..exc import NoAnnotationError
 
 
 class BaseLogParser(object):
@@ -263,9 +264,9 @@ class BaseLogParser(object):
         :param ann_id: The key with which to retrieve the specific annotation file from files['annotations']
         :return: (float, int, str) corresponding to (time, word number, word)
         """
-        # Don't worry if we can't find the annotation file
+        # Worry if we can't find the annotation file
         if ann_id not in self._ann_files:
-            return []
+            raise NoAnnotationError("Missing %s.ann"%ann_id)
 
         # Read the annotation file, getting everything that matches the regular expression
         ann_file = self._ann_files[ann_id]
