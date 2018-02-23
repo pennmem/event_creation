ef91234afabafcfccf21cdfecb9fffdf6a5a8d3a
leond <lad258@cornell.edu>
2018-02-23 16:11:35 -0500

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 import setuptools
 
+install_requirements = []
+try:
+    import yaml
+
+    install_requirements = yaml.load(open('conda_environment.yml'))
+    install_requirements = install_requirements['dependencies'][:-1] + install_requirements['dependencies'][-1]['pip']
+    # Since they haven't been uploaded to PyPI yet:
+    install_requirements.remove('ptsa')
+    install_requirements.remove('bptools')
+
+except ImportError:
+    print("Could not load conda_environment.yml; some requirements may be missing")
 
 setuptools.setup(
     name='event_creation',
@@ -10,6 +22,7 @@ setuptools.setup(
                            "split=event_creation:split"]
     },
     zip_safe = False,
+    install_requires = install_requirements,
     package_data = {'':['*.yml'],
                   'event_creation.submission':['*.json'],
                   'event_creation.submission.readers':['nk*'],
