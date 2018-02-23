a67fb34ee8741025ce2649adbbf40e7069c5f281
leond <lad258@cornell.edu>
2018-02-23 14:47:59 -0500

    refactored convenience

---
+++ event_creation/__init__.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import os,socket
+import runpy
+import sys
+if sys.version_info[0] < 3:
+    input = raw_input
+
+
+def confirm(prompt):
+    while True:
+        resp = input(prompt)
+        if resp.lower() in ('y', 'n', 'yes', 'no'):
+            return resp.lower() == 'y' or resp.lower() == 'yes'
+        print('Please enter y or n')
+
+
+def submit():
+    usr = os.getlogin()
+    expected_users =["RAM_maint","RAM_clinical:"]
+    if usr not in expected_users:
+        print("This script is meant to be run from RAM_maint not", usr)
+        confirm("Are you sure you want to continue? ")
+    host = socket.gethostname()
+    if "node" not in host:
+        print("This script is best run from a node not the headnode (use qlogin)")
+        confirm("Are you sure you want to continue? ")
+    runpy._run_module_as_main("submission.convenience")
+
+
+def split():
+    runpy._run_module_as_main("submission.readers.eeg_splitter")
+
+
