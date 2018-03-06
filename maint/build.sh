e123208254690c1b6470a512f079d76ba207d750
Leon Davis <lad258@cornell.edu>
2018-03-06 11:41:23 -0500

    dependency tracking needs conda v4

--- maint/build.sh
+++ maint/build.sh
@@ -2,4 +2,4 @@
 
 EVENT_CREATION_ROOT=`dirname $0`/../
 export DEPENDENCIES=`sed -ne '/^dependencies:$/{s///; :a' -e 'n;p;ba' -e '}' ${EVENT_CREATION_ROOT}/conda_environment.yml`
-conda build -c pennmem --output-folder  "${EVENT_CREATION_ROOT}/build/" "${EVENT_CREATION_ROOT}/conda.recipe"
+conda build -c pennmem -c conda-forge --output-folder  "${EVENT_CREATION_ROOT}/build/" "${EVENT_CREATION_ROOT}/conda.recipe"
--- setup.py
+++ setup.py
@@ -2,18 +2,6 @@ import setuptools
 from event_creation import __version___
 
 
-install_requirements = []
-try:
-    import yaml
-
-    install_requirements = yaml.load(open('conda_environment.yml'))
-    install_requirements = install_requirements['dependencies'][:-1] + install_requirements['dependencies'][-1]['pip']
-    # Since they haven't been uploaded to PyPI yet:
-    install_requirements.remove('ptsa')
-    install_requirements.remove('bptools')
-
-except ImportError:
-    print("Could not load conda_environment.yml; some requirements may be missing")
 
 setuptools.setup(
     name='event_creation',
@@ -27,7 +15,7 @@ setuptools.setup(
                            "split=event_creation:split"]
     },
     zip_safe = False,
-    # install_requires = install_requirements,
+
     package_data = {'':['*.yml'],
                   'event_creation.submission':['*.json'],
                   'event_creation.submission.readers':['nk*'],
