089d660b556465009fb333e338baec8ddb07ac2c
Leon Davis <lad258@cornell.edu>
2018-03-08 14:26:11 -0500

    removed main/build.sh (superceded by maint/build.py)

--- maint/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-EVENT_CREATION_ROOT=`dirname $0`/../
-export DEPENDENCIES=`sed -ne '/^dependencies:$/{s///; :a' -e 'n;p;ba' -e '}' ${EVENT_CREATION_ROOT}/conda_environment.yml`
-conda build -c pennmem -c conda-forge --output-folder  "${EVENT_CREATION_ROOT}/build/" "${EVENT_CREATION_ROOT}/conda.recipe"
