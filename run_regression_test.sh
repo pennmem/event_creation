3e60559535e5c5edb1268cc91c02fe66dbf08f11
leond <lad258@cornell.edu>
2018-03-12 16:43:11 -0400

    moved util.py into quality

--- run_regression_test.sh
+++ run_regression_test.sh
@@ -2,9 +2,10 @@
 #$ -q RAM.q
 #$ -cwd
 #$ -N regression_events
+#$ -o tests/regression_tests.log
 source ~/.bashrc
 cd ~/event_creation
 rm *.png
 source activate event_creation
 
-python -m tests.regression_tests --db-root=/scratch/db_root $@
+python -m event_creation.tests.regression_tests --db-root=/scratch/leond $@
