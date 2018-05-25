4a4eea0e405941829252ef6d7b14d4957173a85e
jpazdera <jpazdera@haverford.edu>
2018-05-25 11:12:54 -0400

    Initial implementation of the VFFR log parser

--- event_creation/submission/parsers/vffr_log_parser.py
+++ event_creation/submission/parsers/vffr_log_parser.py
@@ -1,30 +1,17 @@
-from .base_log_parser import BaseUnityLTPLogParser
 import numpy as np
+from . import dtypes
+from .base_log_parser import BaseUnityLTPLogParser
+
 
 class VFFRSessionLogParser(BaseUnityLTPLogParser):
-
-    @classmethod
-    def _vffr_fields(cls):
-        """
-        Returns the templates for all LTPFR-specific fields (those not included under _BASE_FIELDS)
-        :return:
-        """
-        return (
-            ('trial', -999, 'int16'),
-            ('serialpos', -999, 'int16'),
-            ('item_name', '', 'S16'),
-            ('recalled', False, 'b1'),
-            ('rectime', -999, 'int32'),
-            ('intrusion', -999, 'int16'),
-            ('eogArtifact', -1, 'int8')
-        )
 
     def __init__(self, protocol, subject, montage, experiment, session, files):
         super(VFFRSessionLogParser, self).__init__(protocol, subject, montage, experiment, session, files)
         self._trial = 0
         self._serialpos = 0
+        self.current_word = ''
 
-        self._add_fields(*self._vffr_fields())
+        self._add_fields(*dtypes.vffr_fields)
         self._add_type_to_new_event(
             end_message=self._event_skip,
             final_recall_prompt=self._event_skip,
@@ -50,7 +37,7 @@ class VFFRSessionLogParser(BaseUnityLTPLogParser):
         )
         self._add_type_to_modify_events(
             final_recall_start=self.modify_ffr,  # Parse FFR annotations and add recall information to events
-            recall_start=self.modify_voc  # Parse annotation for word vocalization and add a vocalization event
+            recall_start=self.modify_rec  # Parse annotation for word vocalization and add a vocalization event
         )
 
     ###############
@@ -61,6 +48,7 @@ class VFFRSessionLogParser(BaseUnityLTPLogParser):
 
     def event_word_on(self, evdata):
         self._serialpos += 1
+        self.current_word = evdata['displayed text']
         event = self.event_default(evdata)
         event.type = 'WORD'
         event.item_name = evdata['displayed text']
@@ -77,12 +65,14 @@ class VFFRSessionLogParser(BaseUnityLTPLogParser):
     def event_rec_start(self, evdata):
         event = self.event_default(evdata)
         event.type = 'REC_START'
+        event.item_name = self.current_word
         event.serialpos = self._serialpos
         return event
 
     def event_rec_stop(self, evdata):
         event = self.event_default(evdata)
         event.type = 'REC_STOP'
+        event.item_name = self.current_word
         event.serialpos = self._serialpos
         return event
 
@@ -111,70 +101,83 @@ class VFFRSessionLogParser(BaseUnityLTPLogParser):
     # EVENT MODIFIER FUNCTIONS
     #
     ###############
-    def modify_voc(self, events):
-        return events
+    def modify_rec(self, events):
 
-    def modify_ffr(self, events):
-        return events
-
-    def modify_recalls(self, events):
-        if self._distractor != 0:
-            events = self.modify_final_distractor_info(self._trial, events).view(np.recarray)
-
+        # Access the REC_START event for this recall period to determine the timestamp when the recording began
         rec_start_event = events[-1]
         rec_start_time = rec_start_event.mstime
 
-        # Get list of recalls from the .ann file for the current list; each recall is (rectime, item_num, item_name)
-        ann_outputs = self._parse_ann_file(str(self._trial - 1))
+        # Get list of recalls from the .ann file for the current word, formatted as (rectime, item_num, item_name)
+        ann_outputs = self._parse_ann_file(str(self._serialpos - 1))
 
+        # For each word in the annotation file (note: there should typically only be one word per .ann in VFFR)
         for recall in ann_outputs:
+
+            # Get the vocalized word from the annotation
             word = recall[-1]
+
+            # Create a new event for the recall
             new_event = self._empty_event
-            new_event.trial = self._trial
-            new_event.session = self._session
+            new_event.type = 'REC_WORD_VV' if word == '<>' or word == 'V' or word == '!' else 'REC_WORD'
+
+            # Get onset time of vocalized word, both relative to the start of the recording, as well as in Unix time
             new_event.rectime = int(round(float(recall[0])))
             new_event.mstime = rec_start_time + new_event.rectime
-            new_event.msoffset = 20
+
+            # Get the vocalized word from the annotation; intrusions probably won't happen, but mark them if they do
             new_event.item_name = word
-            new_event.item_num = int(recall[1])
-            # Vocalizations are skipped in the .mat files for ltpFR2
-            if word == '<>' or word == 'V' or word == '!':
-                new_event.type = 'REC_WORD_VV'
-            else:
-                new_event.type = 'REC_WORD'
+            new_event.intrusion = word != self.current_word
 
-            # If XLI
-            if recall[1] == -1:
-                new_event.intrusion = -1
-            else:  # Correct recall or PLI or XLI from later list
-                # Determines which list the recalled word was from (gives [] if from a future list)
-                pres_mask = self.find_presentation(new_event.item_num, events)
-                pres_trial = np.unique(events[pres_mask].trial)
+            # Record the serial position of the word with the 576-item list
+            new_event.serialpos = self._serialpos
 
-                # Correct recall or PLI
-                if len(pres_trial) == 1:
-                    # Determines how many lists back the recalled word was presented
-                    new_event.intrusion = self._trial - pres_trial[0]
-                    # Retrieve the recalled word's serial position in its list, as well as the distractor(s) used
-                    new_event.serialpos = np.unique(events[pres_mask].serialpos)
-                    new_event.begin_distractor = np.unique(events[pres_mask].begin_distractor)
-                    new_event.final_distractor = np.unique(events[pres_mask].final_distractor)
-                    # Correct recall if word is from the most recent list
-                    if new_event.intrusion == 0:
-                        # Retroactively log on the word pres event that the word was recalled
-                        if not any(events[pres_mask].recalled):
-                            events.recalled[pres_mask] = True
-                    else:
-                        events.intruded[pres_mask] = new_event.intrusion
-                else:  # XLI from later list
-                    new_event.intrusion = -1
-
-            # Add recall event to events array
+            # Append the new event
             events = np.append(events, new_event).view(np.recarray)
 
         return events
 
-    @staticmethod
-    def find_presentation(item_num, events):
-        events = events.view(np.recarray)
-        return np.logical_and(events.item_num == item_num, events.type == 'WORD')
+    def modify_ffr(self, events):
+
+        # Access the REC_START event for this recall period to determine the timestamp when the recording began
+        rec_start_event = events[-1]
+        rec_start_time = rec_start_event.mstime
+
+        # Get list of recalls from the .ann file for the current word, formatted as (rectime, item_num, item_name)
+        ann_outputs = self._parse_ann_file(str(self._serialpos - 1))
+
+        # For each word in the annotation file (note: there should typically only be one word per .ann in VFFR)
+        for recall in ann_outputs:
+
+            # Get the recalled word from the annotation
+            word = recall[-1]
+
+            # Create a new event for the recall
+            new_event = self._empty_event
+            new_event.type = 'FFR_REC_WORD_VV' if word == '<>' or word == 'V' or word == '!' else 'FFR_REC_WORD'
+
+            # Get onset time of vocalized word, both relative to the start of the recording, as well as in Unix time
+            new_event.rectime = int(round(float(recall[0])))
+            new_event.mstime = rec_start_time + new_event.rectime
+
+            # Get the recalled word
+            new_event.item_name = word
+
+            # Determine where the item was presented
+            pres_mask = (events.item_name == word) & (events.type == 'WORD')
+
+            # Correct recall if word was previously presented
+            if pres_mask.sum() == 1:
+                new_event.serialpos = events[pres_mask].serialpos[0]
+                events.recalled[pres_mask] = True
+            # ELI if word was never presented
+            elif pres_mask.sum() == 0:
+                new_event.intrusion = True
+            # If a word was presented multiple times, abort event creation, as this indicates an error with the session
+            else:
+                raise Exception('Word %s was presented multiple times during %s %s! Aborting event creation...' %
+                                (word, self._subject, self._session))
+
+            # Append the new event
+            events = np.append(events, new_event).view(np.recarray)
+
+        return events
