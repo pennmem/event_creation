39c7968acd6f0e90b33cbff8f7bd5ea68a64be66
jpazdera <jpazdera@haverford.edu>
2018-05-25 11:11:42 -0400

    Removed code for abandoned artifact detection methods & updated docstrings

--- event_creation/submission/detection/artifact_detection.py
+++ event_creation/submission/detection/artifact_detection.py
@@ -85,81 +85,19 @@ class ArtifactDetector:
                 self.eeg_mask[self.leog_ind] = False
 
                 # Run artifact detection
-                #self.mark_artifacts()
                 self.mark_bad_epochs()
 
         return self.events
 
-    def mark_artifacts(self):
-
-        # Access currently selected EEG file
-        eeg = self.eeg[self.eegfile]
-
-        # Find the interquartile range of each channel
-        p75 = np.percentile(eeg._data, 75, axis=1)
-        p25 = np.percentile(eeg._data, 25, axis=1)
-        iqr = p75 - p25
-
-        # Set the artifact threshold as 3 * IQR outside of the interquartile range
-        tpos = p75 + 3 * iqr
-        tneg = p25 - 3 * iqr
-
-        # Mark the locations of artifacts on each channel
-        art = np.zeros(eeg._data.shape, dtype=bool)
-        for i in range(eeg._data.shape[0]):
-            art[i, :] = (eeg._data[i, :] - p75[i]) > tpos[i]
-            art[i, :] |= (eeg._data[i, :] - p25[i]) < tneg[i]
-
-        left_eog_art = art[self.leog_ind, :]
-        right_eog_art = art[self.reog_ind, :]
-
     def mark_bad_epochs(self):
         """
-        Runs several bad epoch detection tests -- some on individual channels and others across channels. The test
-        scores and automatically marked bad epochs/bad channels on each epoch are marked in the events structure. The
-        detection methods for cross-channel bad epochs are as follows:
+        Blink/eye movement detection is performed by applying the following method to each EOG channel. The
+        interquartile range of voltages on a given channel is calculated across all time points during all presentation
+        events. Positive and negative voltage thresholds are then set at 3 interquartile ranges above the 75th
+        percentile and 3 interquartile ranges below the 25th percentile. An event is marked as having a blink if the
+        voltage on any EOG channel exceeds either threshold.
 
-        1) Variance during the event, calculated for each channel then averaged across channels. This method is
-        designed to identify events which have large amounts of noise across many or all channels.
-
-        2) Amplitude range during the event, calculated for each channel then averaged across channels. This method is
-        designed to identify events contaminated with high-amplitude artifacts or large baseline shifts across many or
-        all channels.
-
-        Entire presentation events are automatically marked as bad if the average variance across channels (z-scored
-        across events) is greater than 3 or if the average amplitude range across channels (z-scored across events) is
-        greater than 3. Note that both of these methods are adapted from the "FASTER" method by Nolan, Whelan, and
-        Reilly (2010).
-
-        The detection methods for marking individual channels as bad during each event are as follows:
-
-        1) Variance of the channel during the event. Extremely high variance indicates that the channel was noisy during
-        the event.
-
-        2) Median slope of the channel during the event. A high median slope is characteristic of high-frequency
-        artifacts that often originate from muscle activity.
-
-        3) Amplitude range of the channel during the event. A high amplitude range may be indicative of a high-amplitude
-        artifact or a large shift in baseline.
-
-        4) Max/min deviation of the voltage from the channel's interquartile range. The interquartile range is
-        calculated for each channel across all time points and presentation events. Deviation is measured as the maximum
-        number of interquartile ranges above the 75th percentile or below the 25th percentile that the channel's voltage
-        reaches during an event. This is an effective method for identifying high-amplitude artifacts, including blinks.
-
-        Individual channels on each event are marked as bad if the variance of the channel (z-scored across events) is
-        greater than 3, if the median slope of the channel during the event (z-scored across events) is greater than 3,
-        if the amplitude range of the channel (z-scored across events) is greater than 3, and if the channel's voltage
-        reaches 3 IQRs above its 75th percentile/below its 25th percentile at any point during the event. Methods 1, 2,
-        and 3 were based on the methods used in "FASTER".
-
-        Blink/eye movement detection is performed by applying method 4 to each EOG channel. Events where an EOG channel
-        exceeds the 3*IQR threshold are marked as having an EOG artifact.
-
-        The scores and automatic artifact markings for each word presentation are saved into the events structure
-        directly.
-
-        Bad epoch detection does not currently support event types other than word presentations, but could potentially
+        Blink detection does not currently support event types other than word presentations, but could potentially
         be modified to do so.
 
         :return: None
@@ -212,19 +150,7 @@ class ArtifactDetector:
 
         # Apply baseline correction on epoch data before analyzing individual channels across events
         ep.apply_baseline((0, None))
-        """
-        # Method 1: High variance on individual channels during event
-        variance = np.var(ep._data, axis=2)
-        avg_variance = variance[:, self.eeg_mask].mean(axis=1)
 
-        # Method 2: High median slope for individual channels during event
-        gradient = np.gradient(ep._data, axis=2)
-        gradient = np.median(gradient, axis=2)
-
-        # Method 3: High voltage range on individual channels during event
-        amp_range = ep._data.max(axis=2) - ep._data.min(axis=2)
-        avg_amp_range = amp_range[:, self.eeg_mask].mean(axis=1)
-        """
         # Method 4: Large deviation of voltage from interquartile range on individual channels during event
         # Find the interquartile range of each channel, across time and across all events
         p75 = np.percentile(ep._data, 75, axis=[2, 0])
@@ -235,14 +161,7 @@ class ArtifactDetector:
         amp_max_iqr[amp_max_iqr < 0] = 0
         amp_min_iqr = (ep._data.min(axis=2) - p25) / iqr
         amp_min_iqr[amp_min_iqr > 0] = 0
-        """
-        # Mark entire events as bad if they have a high voltage range or variance across channels
-        bad_epoch = np.logical_or(ss.zscore(avg_amp_range) > 3, ss.zscore(avg_variance) > 3)
 
-        # Create events x channels matrices of booleans indicating whether each EEG channel is bad during each event
-        eeg_art = np.logical_or.reduce((ss.zscore(variance, axis=0) > 3, ss.zscore(gradient, axis=0) > 3,
-                                    ss.zscore(amp_range, axis=0) > 3, amp_max_iqr > 3, amp_min_iqr < -3))
-        """
         # Use only method 4 to search for blinks/eye movements in each EOG channel
         right_eog_art = np.logical_or(amp_max_iqr[:, self.reog_ind] > 3, amp_min_iqr[:, self.reog_ind] < -3)
         left_eog_art = np.logical_or(amp_max_iqr[:, self.leog_ind] > 3, amp_min_iqr[:, self.leog_ind] < -3)
@@ -262,24 +181,6 @@ class ArtifactDetector:
         # Also skip any events that run beyond the bounds of the EEG file
         event_mask = event_mask[truncated_events_pre:]
         event_mask = event_mask[:-truncated_events_post] if truncated_events_post > 0 else event_mask
-
-        """
-        # badEpoch is True if abnormally high range or variance occurs across EEG channels
-        self.events.badEpoch[event_mask] = bad_epoch
-        # artifactChannels is a 128-item array indicating whether each EEG channel is bad during each event
-        self.events.artifactChannels[event_mask, :self.n_chans-2] = eeg_art[:, self.eeg_mask]
-
-        # variance is a 128-item array indicating the variance of each channel during the event
-        self.events.variance[event_mask, :self.n_chans-2] = variance[:, self.eeg_mask]
-        # medGradiant is a 128-item array indicating the median gradient of each channel during the event
-        self.events.medGradient[event_mask, :self.n_chans-2] = gradient[:, self.eeg_mask]
-        # ampRange is a 128-item array indicating the amplitude range of each channel during the event
-        self.events.ampRange[event_mask, :self.n_chans-2] = amp_range[:, self.eeg_mask]
-        # iqrDevMax is a 128-item array how many IQRs above the 75th %ile each channel reaches during the event
-        self.events.iqrDevMax[event_mask, :self.n_chans-2] = amp_max_iqr[:, self.eeg_mask]
-        # iqrDevMin is a 128-item array how many IQRs below the 25th %ile each channel reaches during the event
-        self.events.iqrDevMin[event_mask, :self.n_chans-2] = amp_min_iqr[:, self.eeg_mask]
-        """
 
         # Set eogArtifact to 1 if an artifact was detected only on the left, 2 if only on the right, and 3 if both
         self.events.eogArtifact[event_mask] = 0
