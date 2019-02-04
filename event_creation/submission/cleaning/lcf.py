9f84bd36e71345ee2d733a7724d9ac0496eea1f3
jpazdera <jpazdera@haverford.edu>
2019-02-04 15:22:32 -0500

    Added missing string format

--- event_creation/submission/cleaning/lcf.py
+++ event_creation/submission/cleaning/lcf.py
@@ -373,7 +373,7 @@ def run_split_lcf(inputs):
     i = 0
     while 'skip_start%i' % i in breaks and not np.isnan(breaks['skip_start%i' % i]):
         skip_start = breaks['skip_start%i' % i] - start
-        skip_end = breaks['skip_end%i'] - start
+        skip_end = breaks['skip_end%i' % i] - start
         ignore[skip_start:skip_end+1] = True
         i += 1
 
