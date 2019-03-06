7d5240f6e8920783ca22761d16c9a02adfed6651
jpazdera <jpazdera@haverford.edu>
2019-03-06 11:55:39 -0500

    Removed option for bad channel methods during LCF

--- event_creation/submission/events_tasks.py
+++ event_creation/submission/events_tasks.py
@@ -269,8 +269,8 @@ class EventCreationTask(PipelineTask):
             del aligner
             events = artifact_detector.run()
             # Create a cleaned version of the EEG data using localized component filtering
-            run_lcf(events, artifact_detector.eeg, ephys_dir, method='infomax', highpass_freq=.5,
-                    badchan_method='None', iqr_thresh=3, lcf_winsize=.2)
+            run_lcf(events, artifact_detector.eeg, ephys_dir, method='infomax', highpass_freq=.5, iqr_thresh=3,
+                    lcf_winsize=.2)
             del artifact_detector
         # RAM SPECIFIC PROCESSING - Alignment
         elif self.protocol == 'r1':
