4b340eeb0917fcb7d1b25dd1b9dbf704433802a8
zrentala <109314346+zrentala@users.noreply.github.com>
2025-10-16 17:36:53 -0400

    quick change

--- event_creation/submission/events_tasks.py
+++ event_creation/submission/events_tasks.py
@@ -32,7 +32,7 @@ from .parsers.fr_sys3_log_parser import FRSys3LogParser,catFRSys3LogParser
 from .parsers.repfr_log_parser import RepFRSessionLogParser
 from .parsers.delayrepfr_log_parser import DelayRepFRSessionLogParser
 from .parsers.courier_log_parser import CourierSessionLogParser
-from .parsers.value_courier_log_parser import ValueCourierSessionLogParser
+from .parsers.valuecourier_log_parser import ValueCourierSessionLogParser
 from .parsers.nicls_log_parser import NICLSSessionLogParser
 from .parsers.courierreinstate1_log_parser import CourierReinstate1SessionLogParser
 
